d3813d9828a73a96648b8fe423836f554e4b0a4e
weili302 <77049186+weili302@users.noreply.github.com>
2024-11-18 00:45:46 +0800

    Update test.py

--- test.py
+++ test.py
@@ -1,16 +1,18 @@
-import logging
+import re
+import re
 
-# 配置日志记录器，设置日志级别和日志格式
-logging.basicConfig(level=logging.DEBUG,
-                    format='%(asctime)s - %(name)s - %(levelname)s - %(message)s',
-                    handlers=[
-                        logging.FileHandler("your_log_file.log"),  # 文件日志
-                        logging.StreamHandler()  # 控制台日志
-                    ])
+def filter_names(names, full_name):
+    # 将全名拆分为两个部分
+    name1, name2 = full_name.split()
+    # 构建正则表达式模式
+    pattern = re.compile(rf'\b({name1}-\w+[,]? {name2}|{name2}-\w+[,]? {name1}|{name1}[,]? {name2}|{name2}[,]? {name1}|{name1}[,]? {name2}-\w+|{name2}[,]? {name1}-\w+)\b', re.IGNORECASE)
+    filtered_names = [name for name in names if pattern.search(name)]
+    return filtered_names
 
-# 测试日志输出
-logging.debug('这是一个debug信息')
-logging.info('这是一个info信息')
-logging.warning('这是一个warning信息')
-logging.error('这是一个error信息')
-logging.critical('这是一个critical信息')
+if __name__ == "__main__":
+    names = [
+        "a b", "b a", "a-xxx b", "b-xxx a", "a, b", "b, a", "c d", "d c", "a-sd b", "b-edg, a","eca b", "bdf, a"
+    ]
+    full_name = "a b"
+    filtered_names = filter_names(names, full_name)
+    print(filtered_names)
